5777ffcc390dc2a29d96f8eac9ee597fb961a3bd
A_Artemyev <anton.artemyev@hb.by>
2020-08-24 09:42:22 +0300

    init

---
+++ catalog/model/extension/payment/erip_expresspay.php
@@ -0,0 +1,21 @@
+<?php
+class ModelExtensionPaymentEripExpressPay extends Model {
+	public function getMethod($address, $total) {
+		$this->load->language('extension/payment/erip_expresspay');
+		
+		$status = true;
+				
+		$method_data = array();
+		
+		if ($status) {
+			$method_data = array(
+				'code'       => 'erip_expresspay',
+				'title'      => $this->language->get('text_title'),
+				'terms'      => '',
+				'sort_order' => $this->config->get('erip_expresspay_sort_order')
+			);
+		}
+		
+		return $method_data;
+	}
+}
